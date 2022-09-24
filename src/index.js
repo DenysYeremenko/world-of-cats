40d1a039f4afb89179606b0d45b62db1fff7b0a4
Denys Yeremenko <95313654+DenysYeremenko@users.noreply.github.com>
2022-09-24 21:12:13 +0300

    fix: redux store.js path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/world-of-cats/">
     <React.StrictMode>
       <Provider store={store}>

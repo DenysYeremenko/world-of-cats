301b7204225d15b16b560a0eaa498f5548d055cb
Denys Yeremenko <yeremenko.den@gmail.com>
2022-09-24 20:10:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/world-of-cats/">
